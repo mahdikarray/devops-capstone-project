c32a9b73519cbdd69d61419269e18c92063d7bc6
mahdikarray <karraymahdi0@gmail.com>
2026-06-28 21:37:24 -0400

    commit

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -184,7 +184,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         # Check for the CORS header
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-def test_get_account_not_found(self):
+    def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
